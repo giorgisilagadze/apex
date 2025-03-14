174b0687a6ce05b94964bab6ed88ee03802421a0
giorgisilagadze <silagadze.gio123@gmail.com>
2025-03-14 13:31:34 +0400

    done

--- components/card/ProjectCard.tsx
+++ components/card/ProjectCard.tsx
@@ -6,18 +6,20 @@ export default function ProjectCard() {
   const locale = useLocale();
   return (
     <div className="w-full flex flex-col gap-4">
-      <div className="w-full xl:h-[350px] lg1250:h-[300px] lg:h-[350px] md600:h-[300px] h-[350px]  relative">
-        <Image
-          src={"/images/card.png"}
-          alt="card-image"
-          layout="fill"
-          objectFit="cover"
-          className="rounded-[10px] cursor-pointer"
-        />
-        <div className="absolute top-0 right-0 rounded-tr-[10px] rounded-bl-[10px] bg-black px-5 py-2">
-          <p className="text-[14px] text-white font-light">პროექტები</p>
+      <Link href={`/${locale}/news/10`}>
+        <div className="w-full xl:h-[350px] lg1250:h-[300px] lg:h-[350px] md600:h-[300px] h-[350px]  relative">
+          <Image
+            src={"/images/card.png"}
+            alt="card-image"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-[10px] cursor-pointer"
+          />
+          <div className="absolute top-0 right-0 rounded-tr-[10px] rounded-bl-[10px] bg-black px-5 py-2">
+            <p className="text-[14px] text-white font-light">პროექტები</p>
+          </div>
         </div>
-      </div>
+      </Link>
       <div className="w-full flex flex-col gap-1">
         <p className="text-[14px] text-blue">07.02.2025</p>
         <p className="text-[20px] font-bold">
